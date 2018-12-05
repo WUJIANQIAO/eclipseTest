ad32d27fbd24daf91e5e137a2130ec597fa04210
wjq_sys_pro <13068504965@163.com>
2018-12-05 14:15:01 +0800

    123123

---
+++ src/main/java/com/wjq/myRule/WJQBranchMain123.java
@@ -0,0 +1,14 @@
+package com.wjq.myRule;
+
+public class WJQBranchMain123 {
+	
+	public static void main(String[] args) {
+		System.out.println("WJQBranchMain123.main()");
+		System.out.println("WJQBranchMain123.main()");
+		System.out.println("WJQBranchMain123.main()");
+		System.out.println("WJQBranchMain123.main()");
+		System.out.println("WJQBranchMain123.main()");
+		
+	}
+	
+}
